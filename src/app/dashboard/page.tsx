7fbbbb75fa4d6cc58c9dffd88c69f950ca6d89e5
nopaldonh <nopaldonh21@gmail.com>
2025-08-13 13:43:27 +0700

    Creating the Sidebar Layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Logout } from '@/components/logout'
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="p-4">
       <h1>Dashboard</h1>
       <Logout />
     </div>
