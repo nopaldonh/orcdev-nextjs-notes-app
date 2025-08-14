0c58195cf530b8eb541f0cac4ba02250ff0d9bf8
nopaldonh <nopaldonh21@gmail.com>
2025-08-14 20:43:59 +0700

    Notes – Server Actions

---
+++ src/server/notes.tsx
@@ -0,0 +1,42 @@
+'use server'
+
+import { db } from '@/db'
+import { InsertNote, notes } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+
+export const createNote = async (values: InsertNote) => {
+  try {
+    await db.insert(notes).values(values)
+    return { success: true, message: 'Note created successfully' }
+  } catch {
+    return { success: false, message: 'Failed to create notebook' }
+  }
+}
+
+export const getNoteById = async (id: string) => {
+  try {
+    const note = await db.select().from(notes).where(eq(notes.id, id))
+
+    return { success: true, note }
+  } catch {
+    return { success: false, message: 'Failed to get notebook' }
+  }
+}
+
+export const updateNote = async (id: string, values: Partial<InsertNote>) => {
+  try {
+    await db.update(notes).set(values).where(eq(notes.id, id))
+    return { success: true, message: 'Notebook updated successfully' }
+  } catch {
+    return { success: false, message: 'Failed to update notebook' }
+  }
+}
+
+export const deleteNote = async (id: string) => {
+  try {
+    await db.delete(notes).where(eq(notes.id, id))
+    return { success: true, message: 'Notebook deleted successfully' }
+  } catch {
+    return { success: false, message: 'Failed to delete notebook' }
+  }
+}
