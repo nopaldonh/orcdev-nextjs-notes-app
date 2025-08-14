345534db0ade94c7d8cac89746747747fa302289
nopaldonh <nopaldonh21@gmail.com>
2025-08-14 20:03:27 +0700

    Notebooks – Server Actions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import { Logout } from '@/components/logout'
 import { PageWrapper } from '@/components/page-wrapper'
+import { getNotebooks } from '@/server/notebooks'
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const notebooks = await getNotebooks()
+
   return (
     <PageWrapper breadcrumbs={[{ label: 'Dashboard', href: '/dashboard' }]}>
-      <h1>Dashboard</h1>
-      <Logout />
+      <h1>Notebooks</h1>
+
+      {notebooks.success &&
+        notebooks?.notebooks?.map((notebook) => (
+          <div key={notebook.id}>{notebook.name}</div>
+        ))}
+
+      {notebooks.success && notebooks?.notebooks?.length === 0 && (
+        <div>No notebooks found</div>
+      )}
     </PageWrapper>
   )
 }
